8f62c4e3d8ed3e977b608c4db757a0e2a40a9364
Nattaporn Chatmalairut (Thailand) <NattapornCh@internal.local>
2015-12-30 16:57:20 +0700

    Add example for currying

---
+++ ex-currying.js
@@ -0,0 +1,20 @@
+// var dragon = 
+//   name =>
+//     size =>
+//       element =>
+//         name + ' is a ' +
+//         size + ' dragon that breathes ' +
+//         element + '!'
+
+// var output = dragon('Karo')('large')('ice')
+
+// console.log(output)
+
+var dragon = (name, size, element) =>
+  name + ' is a ' +
+  size + ' dragon that breathes ' +
+  element + '!'
+
+
+console.log(dragon('Karo','tiny','lightning'))
+
